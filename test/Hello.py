bb436ebc722c0850fc3c101d82c7833f47cebff7
bhargavi37 <ravalbhargavi0293@gmail.com>
2021-05-28 19:12:49 +0530

    Add basic code of pycham

---
+++ test/Hello.py
@@ -0,0 +1,16 @@
+import time
+
+from selenium import webdriver
+print("hello")
+
+
+#driver=webdriver.Chrome(executable_path="D:\drivers\chromedriver.exe")
+#driver.maximize_window()
+#driver.get("https://www.facebook.com/")
+#print(driver.title)
+#driver.find_element_by_id("email").send_keys("raval.bhargavi@yahoo.com")
+#driver.find_element_by_id("pass").send_keys("02682528764")
+#driver.find_element_by_name("login").click()
+#time.sleep(60)
+
+#driver.close()
